e57e3bf7e02a07dcd64e70c2d91b87ea9e6f9a37
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2024-11-16 16:07:13 +0900

    sidebar追加

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import style from "./Sidebar.module.css";
+import HomeIcon from "@mui/icons-material/Home";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className={style.main_container}>
+      <div className={style.icon_container}>
+        <button>
+          <HomeIcon style={{ fontSize: 32 }} className={style.icon} />
+        </button>
+        <button>
+          <CalendarTodayIcon style={{ fontSize: 32 }} className={style.icon} />
+        </button>
+        <button>
+          <NotificationsIcon style={{ fontSize: 32 }} className={style.icon} />
+        </button>
+      </div>
+      <div className={style.setting_icon_container}>
+        <button>
+          <SettingsIcon
+            style={{ fontSize: 32 }}
+            className={style.icon}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
